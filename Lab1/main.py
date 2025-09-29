ba3fdf89c76479746b4dbfd9f3362ae38342ba6b
user <timofei2005.com@gmail.com>
2025-09-29 17:13:12 +0300

    init

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,4 +1,12 @@
-print( "Hello World!" )
-h = 0
-h = 1+1
-print( h )
+import pandas as pd
+df = pd.read_csv("C:/Users/user/Downloads/spaceship-titanic/test.csv")
+df.info()
+df.dtypes
+print(df.head())
+cols = df.columns
+obj = 'Name'
+if obj in cols:
+    print('Dataset has column Name')
+obj = 'Drowned'
+if obj not in cols:
+    print('Dataset hasn’t column Drowned')
