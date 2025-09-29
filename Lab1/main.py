690f33c0af290199090f54ec393d1d8de65a894a
user <timofei2005.com@gmail.com>
2025-09-29 17:56:17 +0300

    init

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,12 +1,15 @@
 import pandas as pd
-df = pd.read_csv("C:/Users/user/Downloads/spaceship-titanic/test.csv")
+df = pd.read_csv("C:/Users/user/PycharmProjects/AILabs/Lab1/spaceship-titanic/test.csv")
 df.info()
 df.dtypes
-print(df.head())
+print(df.head(100))
 cols = df.columns
-obj = 'Name'
-if obj in cols:
-    print('Dataset has column Name')
-obj = 'Drowned'
-if obj not in cols:
-    print('Dataset hasn’t column Drowned')
+
+nan_matrix = df.isnull()
+print(nan_matrix.head(100))
+print(nan_matrix.sum())
+
+nan_matrix = df.isnull()
+
+cabin_mode = df['Cabin'].mode()[0]
+df['Cabin'].fillna(cabin_mode, inplace=True)
